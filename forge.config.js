5dcbf13e0f3a786d9cad285d1e53b556edb6bb05
mdevans <algorhythmsoftware@icloud.com>
2023-05-10 16:07:11 +0200

    add github action and enable forge publish

--- forge.config.js
+++ forge.config.js
@@ -19,4 +19,16 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'mdevans',
+          name: 'rsna-anonymizer'
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
