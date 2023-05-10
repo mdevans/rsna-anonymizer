b3b8e83bb14b82c6cdd3b263934395509541d933
mdevans <algorhythmsoftware@icloud.com>
2023-05-10 14:23:05 +0200

    initial prototype

--- main.js
+++ main.js
@@ -7,24 +7,17 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        resizable: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
     win.loadFile('index.html')
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
 }
 
 app.whenReady().then(() => {
     createWindow()
-
-    // app.on('activate', () => {
-    //     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    // })
 })
-
-// app.on('window-all-closed', () => {
-//     if (process.platform !== 'darwin') app.quit()
-// })
