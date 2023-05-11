519ec02d5f279b28d6d080b530fb1a546ac6d2bf
mdevans <michael@dx.life>
2023-05-11 16:35:57 +0200

    ... installed maker-dmg

--- forge.config.js
+++ forge.config.js
@@ -16,6 +16,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-dmg',
+      platforms: ['darwin'],
       config: {
         icon: 'assets/icons/rsna.icns',
         overwrite: true,
